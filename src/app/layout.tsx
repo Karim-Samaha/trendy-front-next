20a70b7830ff9ac3ce1208d24bc03691fa25048c
Karim Samaha <kareem.mahmoud1237@gmail.com>
2024-06-13 21:06:54 +0300

    meta-viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,10 +75,12 @@ export default function RootLayout({
         <CartProvider id="cart">
           <html lang="en" dir="" className={cairo.className}>
             <head>
-              <meta
+              {/* <meta
                 name="viewport"
                 content="width=device-width, initial-scale=5"
-              />
+              /> */}
+              <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
             </head>
             <body className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
               <ProgressBar
